8b9d3d97569465fa2d7d9bf09255dc054c87e2d0
ZuXuan. <120767621+FarhanGezon@users.noreply.github.com>
2026-04-01 14:44:53 +0700

    Update pembayaran.php

--- pembayaran.php
+++ pembayaran.php
@@ -1,21 +1,17 @@
 <?php
-#Penggunaan Abstrak Class
 abstract class Pembayaran {
     protected $jumlah;
 
     public function __construct($jumlah) {
         $this->jumlah = $jumlah;
     }
-
-    // method wajib
     abstract public function prosesPembayaran();
 
-    // method umum
     public function validasi() {
         return $this->jumlah > 0;
     }
 
-    // --- TUGAS 2: Tambahan Fitur Diskon 10% dan Pajak 11% ---
+    // Fitur Diskon 10% dan Pajak 11%
     public function hitungDiskon() {
         return $this->jumlah * 0.10; // Diskon 10%
     }
